e5e5bb9208f32a5608fa7a2a8d24a75ce654939e
meteorboy321 <meteorboy321@gmail.com>
2017-11-10 17:28:29 +0800

    傳送簡訊;傳送Email小程式改動，IP不被擋的情況下下載所有永慶房屋資訊

---
+++ src/basic/try122333132132.java
@@ -0,0 +1,33 @@
+package basic;
+
+public class try122333132132 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i =0;i<17;i++){
+			
+			if(i==0||i%4==0){
+				System.out.println("<div class='item'>");
+				System.out.println("內容物"+i);
+				System.out.println("內容物"+i);
+				System.out.println("內容物"+i);
+				System.out.println("內容物"+i);
+				System.out.println("==================");
+				
+			}else if(i!=1||i%4!=0){
+				System.out.println("內容物5 "+i);
+				System.out.println("內容物6 "+i);
+				System.out.println("內容物7 "+i);
+				System.out.println("內容物8 "+i);
+				System.out.println("------------------");
+				if(i==3||i%4==3){
+					System.out.println("<div>");
+				}
+			}
+			
+			
+		}
+	}
+
+}
