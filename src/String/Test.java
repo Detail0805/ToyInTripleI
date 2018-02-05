0a81552868032c93bbd9174c1544bfab108708f2
USER <USER@LAPTOP-UU64VT93.lan>
2018-02-05 19:05:30 +0800

    stream

--- src/String/Test.java
+++ src/String/Test.java
@@ -2,8 +2,8 @@ package String;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 
 public class Test {
@@ -17,50 +17,48 @@ public class Test {
 	};
 	Runnable runnbale = () -> System.out.println("run me!");
 
+	int i = 0;
+
 	public void listAdd() {
 		List<String> list = new ArrayList<String>();
 		list.add("1");
-		list.add("2");
-		list.add("3");
-		list.add("4");
 		list.add("5");
+		list.add("2");
+		list.add("4");
+		list.add("3");
 		System.out.println("list to list :" + list);
-		System.out.println(list.stream().filter(s -> Integer.valueOf(s) < 3).mapToInt(s -> Integer.valueOf(s)).average()
-				.getAsDouble());
-		System.out.println(list.stream().filter(s -> Integer.valueOf(s) < 3).mapToInt(s -> ci.changeNum(s)).average());
-		IntStream is = list.stream().filter(s -> Integer.valueOf(s) > 6).mapToInt(s -> Integer.valueOf(s).parseInt(s));
-		System.out.println("is :" + is);
-		System.out.println(list.stream().filter(s -> Integer.valueOf(s) > 3).collect(Collectors.toList()));
+		List<Double> hihi = new ArrayList<Double>();
+		hihi.add(1.0);
+		hihi.add(2.0);
+		hihi.add(3.0);
+		hihi.add(4.0);
+		hihi.add(5.0);
+		list.forEach(s -> {
+			System.out.println("每次都會做這件事情" + i++);
+			System.out.println("第二次ㄏㄏ 爽");
+		});
 
-		list.stream().collect(Collectors.toList());
+		List<Double> total = list.stream().map(a -> Double.valueOf(a)).collect(() -> new ArrayList<>(),
+				(newList, resource) -> newList.add(resource), (totalList, newList) -> totalList.addAll(newList));
+		System.out.println("total :" + total);
+
+		System.out.println("filter :" + list.stream().filter(s -> Integer.valueOf(s) < 3)
+				.mapToInt(s -> Integer.valueOf(s)).average().getAsDouble());
+		OptionalDouble op = list.stream().filter(s -> Integer.valueOf(s) > 0).mapToInt(s -> ci.changeNum(s)).average();
+		List op2 = (List) list.stream().filter(s -> Integer.valueOf(s) > 0).sorted().collect(Collectors.toList());
+		System.out.println("op :" + op);
+		System.out.println("op2 :" + op2);
+		System.out.println("op.getAsDouble() :" + op.getAsDouble());
+		System.out.println("op.isPresent()" + op.isPresent());
+		System.out.println("op.isPresent()" + op.orElse(0.0));
+		System.out.println("===============");
+		op.ifPresent(s -> System.out.print("我有東西s"));
 	}
 
 	public static void main(String[] args) {
 
 		Test t = new Test();
 		t.listAdd();
-		// List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-		// List<String> liststring = new ArrayList<String>();
-		// Optional<Integer> hi = list.stream().findFirst();
-		// System.out.println("Optione get方法 :" + hi.get());
-		// Stream stream = Stream.of("a", "b", "c");
-
-		// 流的基本構造
-		// IntStream.of(new int[] { 1, 2, 3 }).forEach(System.out::print);
-		// System.out.println();
-		// IntStream.range(1, 3).forEach(System.out::print);
-		// System.out.println();
-		// IntStream.rangeClosed(1, 3).forEach(System.out::print);
-		// // 1. Array
-		// String[] strArray1 = (String[]) stream.toArray(String[]::new);
-		// // 2. Collection
-		// List<String> list1 = (List<String>) stream.collect(Collectors.toList());
-		// List<String> list2 = (List<String>)
-		// stream.collect(Collectors.toCollection(ArrayList::new));
-		// Set set1 = (Set) stream.collect(Collectors.toSet());
-		// Stack stack1 = (Stack) stream.collect(Collectors.toCollection(Stack::new));
-		// // 3. String
-		// String str = stream.collect(Collectors.joining()).toString();
 
 	}
 
